65d90b6f38659d53936351d75b31de1595e4f637
sumuen <zim29144@gmail.com>
2023-04-14 16:43:56 +0800

    ip检测1.1

--- ip变动重启.js
+++ ip变动重启.js
@@ -2,8 +2,8 @@
  * @author Aming
  * @name ip变动重启bncr以及docker
  * @origin muzi
- * @version 1.0.7
- * @description ip变动重启for双拨
+ * @version 1.1.0
+ * @description ip变动重启for双拨，多拨，需要在bncr容器中安装docker，apk add --no-cache docker-cli并重启容器，我是为了重启外部qq,go-cqhttp容器，所以重启go-cqhttp容器，如果你的qq容器名不是go-cqhttp，那么请自行修改
  * @rule ^ip$
  * @priority 1000
  * @admin true
@@ -15,6 +15,8 @@ const axios = require('axios');
 const AmTool = require('./mod/AmTool');
 const sysDB = new BncrDB('system');
 const { exec } = require('child_process');
+const maxIPCount = 2; // 设置 IP 数量（双拨、多拨等）
+//获取ip
 async function getPublicIp() {
   try {
     const response = await axios.get('https://ip.useragentinfo.com/json', { timeout: 10000 }); // 设置 10 秒超时
@@ -25,6 +27,7 @@ async function getPublicIp() {
     return null;
   }
 }
+//重启docker
 function restartContainer(containerNameOrId) {
   exec(`docker restart ${containerNameOrId}`, (error, stdout, stderr) => {
     if (error) {
@@ -34,11 +37,13 @@ function restartContainer(containerNameOrId) {
     }
   });
 }
+//主程序
 module.exports = async s => {
-  const v4DB = (await sysDB.get('publicIpv4')) || [];
+  // 获取数据库中的数据
+  const v4DB = (await sysDB.get('publicIpv4')) || [];//获取数据库中的ip
   const deletedIps = (await sysDB.get('deletedIps')) || [];
   const lastCheckedIp = (await sysDB.get('lastCheckedIp')) || '';
-  const nowV4ip = await getPublicIp();
+  const nowV4ip = await getPublicIp();//获取当前ip
 
   let logs = `上次ip:${(lastCheckedIp && AmTool.Masking(lastCheckedIp, 5, 6)) || '空'}\n`;
   logs += `本次ip:${(nowV4ip && AmTool.Masking(nowV4ip, 5, 6)) || '空'}\n`;
@@ -49,33 +54,42 @@ module.exports = async s => {
     return;
   }
 
-  if (!v4DB.includes(nowV4ip)) {
+  if (v4DB.length < maxIPCount) {
+    // 如果 v4DB中ip数小于多拨数
+    v4DB.push(nowV4ip);
+    await sysDB.set('publicIpv4', v4DB);
+  } else {
+    // 如果 v4DB 不为空，判断当前 IP 是否与 v4DB 中的 IP 相同
+    if (!v4DB.includes(nowV4ip)) {
+      // 如果不同，判断当前 IP 是否在 deletedIps 中
       if (deletedIps.includes(nowV4ip)) {
-        v4DB.push(nowV4ip);
-        deletedIps.splice(deletedIps.indexOf(nowV4ip), 1);
-        await sysDB.set('deletedIps', deletedIps);
+      // 如果在，即代表v4DB中ip误删除
       } else {
         logs += '进行bncr与docker重启...';
         open = true;
-        const removedIp = v4DB.shift();
-        // 如果 deletedIps 长度已经为 1，则移除旧的 IP
-        if (deletedIps.length >= 1) {
+        // 如果不在 deletedIps 中，判断 v4DB 的长度是否大于等于 maxIPCount
+        if (v4DB.length >= maxIPCount) {
+          const removedIp = v4DB.shift();
           deletedIps.shift();
-      }
-        deletedIps.push(removedIp);
-        await sysDB.set('deletedIps', deletedIps);
+          deletedIps.push(removedIp);
+          await sysDB.set('deletedIps', deletedIps);
+        }
         restartContainer('go-cqhttp');
         v4DB.push(nowV4ip);
+        //将当前IP添加到v4DB
+        await sysDB.set('publicIpv4', v4DB); //保存到数据库
       }
-      await sysDB.set('publicIpv4', v4DB);
+      
+    }
   }
-
-  await sysDB.set('lastCheckedIp', nowV4ip);
-//输出日志
+  await sysDB.set('lastCheckedIp', nowV4ip); //保存到数据库
+  
+//输出日志用以纠错
   console.log('v4DB', v4DB);
   console.log('deletedIps', deletedIps);
   console.log('lastCheckedIp', lastCheckedIp);
 
+//回复
   await s.reply(logs);
   open && (s.getFrom() === 'cron' ? sysMethod.inline('重启') : s.inlineSugar('重启'));
 };
