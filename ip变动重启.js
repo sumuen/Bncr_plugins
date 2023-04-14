fe459154eb5eeacbba13c5fc30400c180c6de79a
sumuen <zim29144@gmail.com>
2023-04-14 13:36:31 +0800

    修复bug

--- ip变动重启.js
+++ ip变动重启.js
@@ -2,7 +2,7 @@
  * @author Aming
  * @name ip变动重启bncr以及docker
  * @origin muzi
- * @version 1.0.5
+ * @version 1.0.7
  * @description ip变动重启for双拨
  * @rule ^ip$
  * @priority 1000
@@ -40,34 +40,41 @@ module.exports = async s => {
   const lastCheckedIp = (await sysDB.get('lastCheckedIp')) || '';
   const nowV4ip = await getPublicIp();
 
+  let logs = `上次ip:${(lastCheckedIp && AmTool.Masking(lastCheckedIp, 5, 6)) || '空'}\n`;
+  logs += `本次ip:${(nowV4ip && AmTool.Masking(nowV4ip, 5, 6)) || '空'}\n`;
+  let open = false;
+
   if (nowV4ip === null) {
     // 获取 IP 失败，不执行后续操作
     return;
   }
 
-  let logs = `上次ip:${(lastCheckedIp && AmTool.Masking(lastCheckedIp, 5, 6)) || '空'}\n`;
-  logs += `本次ip:${(nowV4ip && AmTool.Masking(nowV4ip, 5, 6)) || '空'}\n`;
-  let open = false;
-
-  if (lastCheckedIp !== nowV4ip) {
-    if (deletedIps.includes(nowV4ip)) {
-      v4DB.push(nowV4ip);
-      deletedIps.splice(deletedIps.indexOf(nowV4ip), 1);
-      await sysDB.set('deletedIps', deletedIps);
-    } else if (!v4DB.includes(nowV4ip)) {
-      if (v4DB.length >= 2) {
+  if (!v4DB.includes(nowV4ip)) {
+      if (deletedIps.includes(nowV4ip)) {
+        v4DB.push(nowV4ip);
+        deletedIps.splice(deletedIps.indexOf(nowV4ip), 1);
+        await sysDB.set('deletedIps', deletedIps);
+      } else {
         logs += '进行bncr与docker重启...';
         open = true;
         const removedIp = v4DB.shift();
+        // 如果 deletedIps 长度已经为 1，则移除旧的 IP
+        if (deletedIps.length >= 1) {
+          deletedIps.shift();
+      }
         deletedIps.push(removedIp);
         await sysDB.set('deletedIps', deletedIps);
         restartContainer('go-cqhttp');
+        v4DB.push(nowV4ip);
       }
-      v4DB.push(nowV4ip);
-    }
-    await sysDB.set('publicIpv4', v4DB);
-    await sysDB.set('lastCheckedIp', nowV4ip);
+      await sysDB.set('publicIpv4', v4DB);
   }
+
+  await sysDB.set('lastCheckedIp', nowV4ip);
+//输出日志
+  console.log('v4DB', v4DB);
+  console.log('deletedIps', deletedIps);
+  console.log('lastCheckedIp', lastCheckedIp);
 
   await s.reply(logs);
   open && (s.getFrom() === 'cron' ? sysMethod.inline('重启') : s.inlineSugar('重启'));
