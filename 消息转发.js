5eab76333ac87d4444829983ee5d7fe6695e3e0c
sumuen <zim29144@gmail.com>
2023-05-13 02:48:12 +0800

    消息抓发

--- 消息转发.js
+++ 消息转发.js
@@ -4,7 +4,7 @@
  * @origin 红灯区
  * @version v1.0.0
  * @name 消息转发
- * @rule 转发[\s\S]+
+ * @rule [\s\S]+
  * @priority 100000
  * @admin false
  * @disable false
@@ -19,21 +19,17 @@ const configs = [
 	{
 		listen: {
 			/* 监听的群 */
-			id: 277243301,
-			from: 'qq',
+			id: -1001897135102,
+			from: 'pgm',
 			type: 'groupId',
 		},
-		rule: ['你好', '明天', '测试'] /* 触发到关键词将进行转发 */,
+		rule: ['export','https'] /* 触发到关键词将进行转发 */,
+		headText: `spy插队\n`, // 新增的头部信息
 		toSender: [
 			/* 转发目的地,可以是人,可以是群,可多个 */
 			{
-				id: 1629887728 /* id */,
-				from: 'tgBot',
-				type: 'userId',
-			},
-			{
-				id: -1001744932665 /* id */,
-				from: 'tgBot',
+				id: -906445479 /* id */,
+				from: 'pgm',
 				type: 'groupId',
 			},
 		],
@@ -46,20 +42,100 @@ const configs = [
 		],
 		addText: `\n\nBncr的转发消息` /* 尾部增加的消息 */,
 	},
+	{
+		listen: {
+			/* 监听的群 */
+			id: -1001895018795, // 新的群组id
+			from: 'pgm',
+			type: 'groupId',
+		},
+		rule: ['https','export'] /* 触发到关键词将进行转发 */,
+		toSender: [
+			/* 转发目的地,可以是人,可以是群,可多个 */
+			{
+				id: -906445479, // 新的目的地id
+				from: 'pgm',
+				type: 'groupId',
+			},
+		],
+		replace: [
+			/* 需要替换的信息 */
+			{
+				old: '你好',
+				new: '你不好',
+			},
+		],
+		addText: `\n\nBncr的转发消息` /* 尾部增加的消息 */,
+	},
+	// {
+	// 	listen: {
+	// 		/* 监听的群 */
+	// 		id: -1001877487436, // 新的群组id
+	// 		from: 'pgm',
+	// 		type: 'groupId',
+	// 	},
+	// 	rule: ['https',] /* 触发到关键词将进行转发 */,
+	// 	toSender: [
+	// 		/* 转发目的地,可以是人,可以是群,可多个 */
+	// 		{
+	// 			id: -906445479, // 新的目的地id
+	// 			from: 'pgm',
+	// 			type: 'groupId',
+	// 		},
+	// 	],
+	// 	replace: [
+	// 		/* 需要替换的信息 */
+	// 		{
+	// 			old: '你好',
+	// 			new: '你不好',
+	// 		},
+	// 	],
+	// 	addText: `\n\nBncr的转发消息` /* 尾部增加的消息 */,
+	// },
+		{
+		listen: {
+			/* 监听的群 */
+			id: -1001897135102, // 新的群组id
+			from: 'pgm',
+			type: 'groupId',
+		},
+		rule: ['抽现金',] /* 触发到关键词将进行转发 */,
+		activeHours: [9, 12], 
+		headText: `spy立即执行\n`, // 新增的头部信息
+		toSender: [
+			/* 转发目的地,可以是人,可以是群,可多个 */
+			{
+				id: -906445479, // 新的目的地id
+				from: 'pgm',
+				type: 'groupId',
+			},
+		],
+		replace: [
+			/* 需要替换的信息 */
+			{
+				old: '你好',
+				new: '你不好',
+			},
+		],
+		addText: `\n export YQCJ_DRAW="true" \nexport YQCJ_CK_START_INDEX="160" # 从第165个号开始助力。`/* 尾部增加的消息 */
+	},
 ];
+
+
 
 /* mian */
 module.exports = async s => {
 	/* 异步处理 */
 	new Promise(resolve => {
 		const msgInfo = s.msgInfo;
-		/* 头部消息 */
-		let head = `来自${msgInfo.from}
-群:${msgInfo.groupId}
-人:${msgInfo.userId}
-
-`;
 		for (const config of configs) {
+			/* 检查当前时间是否在活动时间内 */
+			let now = new Date();
+			let currentHour = now.getHours();
+			let activeHours = config.activeHours || [0, 24]; // 如果没有指定活动时间，就默认整天都是活动时间
+			if (currentHour < activeHours[0] || currentHour >= activeHours[1]) {
+				continue; // 不在活动时间内，跳过这个配置
+			}
 			let msgStr = msgInfo.msg,
 				open = false;
 			if (msgInfo.from !== config.listen.from || +msgInfo[config.listen.type] !== config.listen.id) continue;
@@ -67,7 +143,8 @@ module.exports = async s => {
 				if (msgInfo.msg.includes(rule))
 					(open = true), config.replace.forEach(e => (msgStr = msgStr.replace(new RegExp(e.old, 'g'), e.new)));
 			if (!open) break;
-			msgStr = `${head}${msgStr}${config.addText}`;
+			let headText = config.headText || `来自${msgInfo.from}\n群:${msgInfo.groupId}\n人:${msgInfo.userId}\n\n`; // 使用config.headText代替原先的head，如果没有指定，则使用默认值
+			msgStr = `${headText}${msgStr}${config.addText}`;
 			config.toSender.forEach(e => {
 				let obj = {
 					platform: e.from,
